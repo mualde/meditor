2d2bef7b538b87d31bf31d2dd07fc5686d548759
Muhammed Ali Demirhan <56094596+Mualde@users.noreply.github.com>
2024-12-29 05:36:08 +0300

    Update kaydet-js.js

--- meditor/eklentiler/kaydet/kaydet-js.js
+++ meditor/eklentiler/kaydet/kaydet-js.js
@@ -1,7 +1,6 @@
 function AddkaydetBtn() {
     const kaydetContainer = document.createElement('div');
     kaydetContainer.id = 'kaydet-container';
-    kaydetContainer.className = 'container';
     kaydetContainer.innerHTML = `
         <button id="kaydetBtn" title="Kaydet" onclick="savePost();">
             <i class="fa-solid fa-save" style="color:#18952D; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);"></i>
